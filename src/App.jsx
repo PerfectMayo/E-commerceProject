13877cb4b805aa786a9e884bf83cc6170f7a1db0
Ali <yasinahmadali3@gmail.com>
2026-06-03 11:43:57 +0100

    Initial commit: setting up ecommerce project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Checkout from "./pages/Checkout";
+import NavBar from "./components/NavBar";
+function App() {
+  return (
+    <div className="app">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
